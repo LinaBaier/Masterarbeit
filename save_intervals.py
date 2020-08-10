34a52eda8c3850703b7c399c162259bfe22837f2
LinaBaier <baier_lina@web.de>
2020-08-10 17:53:36 +0200

    with random forest

--- save_intervals.py
+++ save_intervals.py
@@ -24,18 +24,3 @@ def save_intervals(name, intervals):
         del dim0[0], dim0[-1], dim1[0], dim1[-1], dim2[0], dim2[-1]
         with open("Ergebnisse_test.csv","a") as file:
             file.write(name + ";" + str(dim0) + ";" + str(dim1) + ";" + str(dim2) + "\n")
-
-
-      
-
-'''
-    for i in range(0, len(intervals),3):
-        if i = len(intervals)-1:
-            speiche
-        elif intervals
-        dim0, dim1, dim2 = intervals[i].split("\n"), intervals[i+1].split("\n"), intervals[i+2].split("\n")
-        del dim0[0], dim1[0], dim2[0], dim0[-1], dim1[-1], dim2[-1]
-        len0, len1, len2 = str(len(dim0)), str(len(dim1)), str(len(dim2))
-        with open("Ergebnisse.csv","a") as file:
-            file.write(name + ";" + str(dim0) + ";" + str(dim1) + ";" + str(dim2) + ";" + len0 + ";" + len1 + ";" + len2 + "\n")
-'''
