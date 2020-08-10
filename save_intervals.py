cee560872cd57f409a8ddc62f8ff2a2446aefb20
LinaBaier <baier_lina@web.de>
2020-08-10 15:56:13 +0200

    generation of barcode intevals we will use for supervised ML

--- save_intervals.py
+++ save_intervals.py
@@ -4,21 +4,26 @@ def save_intervals(name, intervals):
     i = 0
     while i < len(intervals):
         dim0 = intervals[i].split("\n")
-        if i < len(intervals)-1:
-            dim1 = intervals[i+1].split("\n")
-            dim1 = dim1 if dim1[0] == "1" else ["",""]  
+        i += 1
+        if i < len(intervals):
+            dim1 = intervals[i].split("\n")
+            if dim1[0] == "1":
+                i += 1
+            else:
+                dim1 = ["",""]
         else:
             dim1 = ["",""]
-        if i < len(intervals)-2:
-            dim2 = intervals[i+2].split("\n")
-            dim2 = dim2 if dim2[0] == "2" else ["",""]
+        if i < len(intervals):
+            dim2 = intervals[i].split("\n")
+            if dim2[0] == "2":
+                i += 1
+            else:
+                dim2 = ["",""]
         else:
             dim2 =["",""]
         del dim0[0], dim0[-1], dim1[0], dim1[-1], dim2[0], dim2[-1]
-        len0, len1, len2 = str(len(dim0)), str(len(dim1)), str(len(dim2))
         with open("Ergebnisse_test.csv","a") as file:
-            file.write(name + ";" + str(dim0) + ";" + str(dim1) + ";" + str(dim2) + ";" + len0 + ";" + len1 + ";" + len2 + "\n")
-        i += 3
+            file.write(name + ";" + str(dim0) + ";" + str(dim1) + ";" + str(dim2) + "\n")
 
 
       
